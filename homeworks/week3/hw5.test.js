1126c7a76a9c32949e358e5b573a7154717d315d
Joanne <joannechucheerup@gmail.com>
2019-05-07 14:27:01 +0200

    week3 updated

--- homeworks/week3/hw5.test.js
+++ homeworks/week3/hw5.test.js
@@ -4,4 +4,13 @@ describe('hw5', () => {
   it('should return correct answer when a=111111111111111111111111111111111111 and b=111111111111111111111111111111111111', () => {
     expect(add('111111111111111111111111111111111111', '111111111111111111111111111111111111')).toBe('222222222222222222222222222222222222');
   });
+  it('should return correct answer when a=39 and b=123', () => {
+    expect(add('39', '123')).toBe('162');
+  });
+  it('should return correct answer when a=1 and b=123', () => {
+    expect(add('1', '123')).toBe('124');
+  });
+  it('should return correct answer when a=9999999999 and b=8888888888', () => {
+    expect(add('9999999999', '8888888888')).toBe('18888888887');
+  });
 });
